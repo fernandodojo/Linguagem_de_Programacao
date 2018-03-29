6a9e9573712305748f6c9681fddeb387c7438636
Fernando Henrique <fernandodojo@gmail.com>
2018-03-29 18:00:55 -0300

    final code

--- Exercícios/Jogodavelha/main.cpp
+++ Exercícios/Jogodavelha/main.cpp
@@ -1,24 +1,13 @@
-//Se for necessario armazenar diferente tipos de dados não usar vetor; se for necessario crescer ou diminuir, tbm nã é possivel usar vetor;
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int posi, positemp, i, j, state;
-    int horizontal1x, horizontal1o, horizontal2x=0, horizontal2o=0, horizontal3x=0, horizontal3o=0;
-    int vertical1=0, vertical2=0, vertical3=0;
-    int diagonalp=0, diagonals=0;
+    int posi, i, j, state;
+    char velha[9], simb;
 
-    char velha[9], simb, simbtemp;
 
-    /*cout<<"Digite a posicao: ";
-    cin>>posi;
-    cout<<"Digite o simbolo 'x' ou 'o' ";
-    cin>>simb;
-    //velha[3*x+y]= n;*/
-
-    state = 0;
+    state = 0;//flag
 
     //PRENCHIMENTO DO VETOR PARA EVITAR EMPRESSÃO DE VALOR/CARACTER ALEATORIO GUARDADO NA MEMORIA
     for(i=0; i<9; i++)
@@ -26,23 +15,28 @@ int main()
         velha[i]='#';
     }
 
+    cout<<"MAPA DE POSICOES\n";
+    cout<<"   [1][2][3]\n";
+    cout<<"   [4][5][6]\n";
+    cout<<"   [7][8][9]\n";
+
     do// CRIACAO DE LACO/FLAG, PARA PERMANENCIA NO JOGO
     {
         //SOLICITAÇÃO E VERIFICAÇÃO DO VALOR ATRIBUIDO PELO USUARIO A POSIÇÃO//
-        cout<<"Digite a posicao entre 1 e 9: ";
-        cin>>positemp;
-        if (positemp>=1 && positemp<=9)
-            posi = positemp;
-        else
-            cout<<"DIGITE O NUMERO DA POSICAO ENTRE 1 E 9\n";
+        posi=0;
+        while (posi<1 || posi>9)
+        {
+            cout<<"Digite a posicao entre 1 e 9: ";
+            cin>>posi;
+        }
 
-        //SOLICITAÇÃO E VERIFICAÇÃO DO VALOR ATRIBUIDO PELO USUARIO AO SIMBOLO(X OU O)//
-        cout<<"Digite a letra 'X' ou 'O': ";
-        cin>>simbtemp;
-        if (simbtemp=='x' || simbtemp=='X' || simbtemp=='o' || simbtemp=='O')
-            simb = simbtemp;
-        else
-            cout<<"DIGITE A LETRA 'x', 'X', 'o', 'O'!\n";
+        //SOLICITAÇÃO E VERIFICAÇÃO DO VALOR ATRIBUIDO PELO USUARIO AO SIMBOLO( X OU O)//
+        simb = 'i';
+        while ((simb!='x') && (simb!='o'))
+        {
+            cout<<"Digite a letra 'x' ou 'o': ";
+            cin>>simb;
+        }
 
         //INSERÇÃO DA LETRA X OU O SE E SOMENTE SE A POSIÇÃO ESTIVER DESOCUPADA//
         if (velha[posi-1]== '#')
@@ -50,7 +44,6 @@ int main()
         else
             cout<<"\nTente outra posicao!\n";
         //INSERÇÃO DA LETRA X OU O SE E SOMENTE SE A POSIÇÃO ESTIVER DESOCUPADA//
-
 
         //IMPRESSÃO
         for (j=0; j<3; j++)
@@ -64,6 +57,7 @@ int main()
         cout<<"\n";
         //IMPRESSÃO
 
+        //CONDICOES DE VITORIA DO JOGADOR X
        	if ((velha[0]=='x' && velha[1]=='x' && velha[2]=='x')||
 		(velha[3]=='x' && velha[4]=='x' && velha[5]=='x')||
 		(velha[6]=='x' && velha[7]=='x' && velha[8]=='x')||
@@ -76,7 +70,7 @@ int main()
             cout<<"Jogador 'x' ganhou.";
             state=1;
         }
-
+        //CONDICOES DE VITORIA DO JOGADOR O
         if ((velha[0]=='o' && velha[1]=='o' && velha[2]=='o')||
 		(velha[3]=='o' && velha[4]=='o' && velha[5]=='o')||
 		(velha[6]=='o' && velha[7]=='o' && velha[8]=='o')||
@@ -90,256 +84,8 @@ int main()
             cout<<"Jogador 'o' ganhou.";
             state=1;
         }
-
     }
     while (state==0); //FLAG PARA PERMANENCIA DO JOGO ENQUANTO NINGUEM GANHAR
 
     return 0;
 }
-
-
-
-/*
-       if (velha[0]=='x' && velha[1]=='x' && velha[2]=='x')
-        {
-            cout<<"Jogador 'x' ganhou.";
-            state=1;
-        }
-        else if (velha[0]=='o' && velha[1]=='o' && velha[2]=='o')
-        {
-            cout<<"Jogador 'o' ganhou.";
-            state=1;
-        }
-        /////////////////////////////////////////////////////
-
-        else if (velha[3]=='x' && velha[4]=='x' && velha[5]=='x')
-        {
-            cout<<"Jogador 'x' ganhou.";
-            state=1;
-        }
-        else if (velha[3]=='o' && velha[4]=='o' && velha[5]=='o')
-        {
-            cout<<"Jogador 'o' ganhou.";
-            state=1;
-        }
-        /////////////////////////////////////////////////////
-
-        else if (velha[6]=='x' && velha[7]=='x' && velha[8]=='x')
-        {
-            cout<<"Jogador 'x' ganhou.";
-            state=1;
-        }
-        else if (velha[6]=='o' && velha[7]=='o' && velha[8]=='o')
-         {
-            cout<<"Jogador 'o' ganhou.";
-            state=1;
-        }
-        /////////////////////////////////////////////////////
-
-        else if (velha[0]=='x' && velha[3]=='x' && velha[6]=='x')
-        {
-            cout<<"Jogador 'x' ganhou.";
-            state=1;
-        }
-        else if (velha[0]=='o' && velha[3]=='o' && velha[6]=='o')
-        {
-            cout<<"Jogador 'o' ganhou.";
-            state=1;
-        }
-        /////////////////////////////////////////////////////
-
-        else if (velha[1]=='x' && velha[4]=='x' && velha[7]=='x')
-        {
-            cout<<"Jogador 'x' ganhou.";
-            state=1;
-        }
-        else if (velha[1]=='o' && velha[4]=='o' && velha[7]=='o')
-        {
-            cout<<"Jogador 'o' ganhou.";
-            state=1;
-        }
-        /////////////////////////////////////////////////////
-
-        else if (velha[2]=='x' && velha[5]=='x' && velha[8]=='x')
-        {
-            cout<<"Jogador 'x' ganhou.";
-            state=1;
-        }
-        else if (velha[2]=='o' && velha[5]=='o' && velha[8]=='o')
-        {
-            cout<<"Jogador 'o' ganhou.";
-            state=1;
-        }
-        /////////////////////////////////////////////////////
-
-        else if (velha[0]=='x' && velha[4]=='x' && velha[8]=='x')
-        {
-            cout<<"Jogador 'x' ganhou.";
-            state=1;
-        }
-        else if (velha[0]=='o' && velha[4]=='o' && velha[8]=='o')
-        {
-            cout<<"Jogador 'o' ganhou.";
-            state=1;
-        }
-        /////////////////////////////////////////////////////
-
-        else if (velha[2]=='x' && velha[4]=='x' && velha[6]=='x')
-        {
-            cout<<"Jogador 'x' ganhou.";
-            state=1;
-        }
-        else if (velha[2]=='o' && velha[4]=='o' && velha[6]=='o')
-        {
-            cout<<"Jogador 'o' ganhou.";
-            state=1;
-        }*/
-//contax = 0;
-        //contao = 0;
-
-       /* horizontal1x=0;
-        horizontal1o=0;
-
-        //CHECAGEM HORIZONTAL
-        for(i=0; i<3; i++)
-        {
-            if (velha[i]=='x')
-                horizontal1x = horizontal1x + 1;
-            else if (velha[i]=='o')
-                horizontal1o = horizontal1o + 1;
-
-
-
-        }
-
-        if (horizontal1x == 3)
-            {
-                cout<<"Jogador 'x' ganhou.";
-                state=1;
-            }
-            else if (horizontal1o == 3)
-            {
-                cout<<"Jogador '0' ganhou.";
-                state=1;
-            }
-        for(i=3; i<6; i++)
-        {
-            if (velha[i]=='x')
-                horizontal2 = horizontal2 + 1;
-            if (velha[i]=='o')
-                horizontal2 = horizontal2 + 1;
-            if (horizontal2 == 3)
-            {
-                state=1;
-            }
-        }
-        for(i=6; i<9; i++)
-        {
-            if (velha[i]=='x')
-                contax = contax + 1;
-            if (velha[i]=='o')
-                contao = contao + 1;
-            if (contax == 3)
-            {
-                cout<<"Jogador 'x' ganhou.";
-                state=1;
-            }
-            if (contao == 3)
-            {
-                cout<<"Jogador '0' ganhou.";
-                state=1;
-            }
-        }
-
-        //CHECAGEM VERTICAL
-        for(i=0; i<7; i+=3)
-        {
-            if (velha[i]=='x')
-                contax = contax + 1;
-            if (velha[i]=='o')
-                contao = contao + 1;
-            if (contax == 3)
-            {
-                cout<<"Jogador 'x' ganhou.";
-                state=1;
-            }
-            if (contao == 3)
-            {
-                cout<<"Jogador '0' ganhou.";
-                state=1;
-            }
-        }
-        for(i=1; i<8; i+=3)
-        {
-            if (velha[i]=='x')
-                contax = contax + 1;
-            if (velha[i]=='o')
-                contao = contao + 1;
-            if (contax == 3)
-            {
-                cout<<"Jogador 'x' ganhou.";
-                state=1;
-            }
-            if (contao == 3)
-            {
-                cout<<"Jogador '0' ganhou.";
-                state=1;
-            }
-        }
-        for(i=2; i<9; i+=3)
-        {
-            if (velha[i]=='x')
-                contax = contax + 1;
-            if (velha[i]=='o')
-                contao = contao + 1;
-            if (contax == 3)
-            {
-                cout<<"Jogador 'x' ganhou.";
-                state=1;
-            }
-            if (contao == 3)
-            {
-                cout<<"Jogador '0' ganhou.";
-                state=1;
-            }
-        }
-
-        //CHECAGEM DIAGONA PRINCIPAL
-        for(i=0; i<9; i+=4)
-        {
-            if (velha[i]=='x')
-                contax = contax + 1;
-            if (velha[i]=='o')
-                contao = contao + 1;
-            if (contax == 3)
-            {
-                cout<<"Jogador 'x' ganhou.";
-                state=1;
-            }
-            if (contao == 3)
-            {
-                cout<<"Jogador '0' ganhou.";
-                state=1;
-            }
-        }
-
-        //CHECAGEM DIAGONA SECUNDARIA
-        for(i=2; i<7; i+=2)
-        {
-            if (velha[i]=='x')
-                contax = contax + 1;
-            if (velha[i]=='o')
-                contao = contao + 1;
-            if (contax == 3)
-            {
-                cout<<"Jogador 'x' ganhou.";
-                state=1;
-            }
-            if (contao == 3)
-            {
-                cout<<"Jogador '0' ganhou.";
-                state=1;
-            }
-        }
-
-*/
