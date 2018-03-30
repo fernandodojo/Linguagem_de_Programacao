4fb8cd8be51bcc54336cca2f8d5654ccdb177457
Fernando Henrique <fernandodojo@gmail.com>
2018-03-30 19:43:15 -0300

    Single bullet and meteor fully functional

--- Exercícios/Meteoros/main.cpp
+++ Exercícios/Meteoros/main.cpp
@@ -6,49 +6,55 @@ using namespace std;
 
 int main()
 {
-    int i, j, pontos=10, px = 20, py=21, rd, mx=1, my, bx=0, by=0;
-    char tela[22][41], tecla;
-    //int bullet[10];
+    int i, j, pontos=10;
+    int px = 20, py=21;         //coordenadas x e y do player
+    int rd;                     //numero aleatorio
+    int mx=1, my;               //coordenacas x e y do meteoro
+    int bx=0, by=0;             //coordenadas x e y da bala
+    char tela[22][41], tecla;   //matriz de coordenadas e tecla de movimento
 
-
-    rd = rand()%40;
-    my = rd;
+    rd = rand()%40; //gerador de numero aleatorio entre 1 e 39
+    my = rd;        //coordenada y do meteoro recebe numero aleatorio
 
     do{
+        system("clear");
         cout<<"pontos = "<<pontos;
-        if (mx>=21)
+        if (mx>=21) //decrescimento e reinicialização da posição do meteoro caso o mesmo chegue ao chao
         {
             pontos = pontos-5;
-            mx = 2;
+            mx = 2; //inicio no topo da tela
             rd = rand()%40;
             my = rd;
         }
 
-        mx = mx+1;
+        if(bx<=1)   //desaparece com a bala caso a mesma atinja o topo da tela
+        {
+            bx=0;
+            by=0;
+        }
+
+        mx = mx+1; //aumenta ou diminui a posição vertical da bala e do meteoro
         bx = bx-1;
 
-
-        //cout<<mx<<my;
         for(i=0; i<22; i++)
         {
             for (j=0; j<41; j++)
             {
-                if (i==21 || j==0 || i==1 || j==40)
+                if (i==21 || j==0 || i==1 || j==40) //imprime bordas da tela
                     tela[i][j] ='#';
                 else
-                    tela[i][j] =' ';
+                    tela[i][j] =' ';//preenche qualquer coisa q não seja a borda com espaços
 
-                tela[mx][my] = '*';
-                tela[bx][by] = '|';
-                tela[px][py]= '_';
+                tela[mx][my] = '*';//define a posição do meteoro em todas as interações
+                tela[bx][by] = '|';//define a posição da bala em todas as interações
+                tela[px][py]= '_';// define a posição do player em todas as interações
 
-                cout<<tela[i][j];
+                cout<<tela[i][j]; //impressão da matriz inteira
             }
             cout<<"\n";
 
         }
-        //cout<<py;
-        cin>>tecla;
+        cin>>tecla; // recebimento da letra para realizar uma ação pré programada
 
         //TELETRANPORTE AO SAIR DA TELA
         if (py<=1)
@@ -62,19 +68,39 @@ int main()
             py = py-1;
         if (tecla=='d')
             py = py+1;
+        if (tecla=='s')
+            py = py;
+        //TECLAS DE MOVIMENTO DO PLAYER
+
+        //TECLAS DE MOVIMENTO DA BALA
         if (tecla =='l')
         {
-            pontos = pontos-2
+            pontos = pontos-2;
             bx=21;
             by=py;
             bx = bx-1;
         }
-        if (bx==mx)
-            pontos = pontos + 3
-        //TECLAS DE MOVIMENTO DO PLAYER
+        //TECLAS DE MOVIMENTO DA BALA
 
+        //CONDIÇÃO PARA REINICIALIZAÇÃO DA POSIÇÃO DA BALA E METEORO EM CASO DE COLISÃO
+        if (((bx>=mx-1) && (bx<=mx+1)) && (by==my))
+        {
+            pontos = pontos + 3;
+            mx = 2;
+            rd = rand()%40;
+            my = rd;
+            bx=0;
+            by=0;
+        }
+        //CONDIÇÃO PARA REINICIALIZAÇÃO DA POSIÇÃO DA BALA E METEORO EM CASO DE COLISÃO
+
+    //cout<<mx<<" "<<my<<"\n";
+    //cout<<bx<<" "<<by<<"\n";
     }
     while(pontos>0);
+
+    if(pontos<=0)
+        cout<<"Game Over";
 
 }
 
