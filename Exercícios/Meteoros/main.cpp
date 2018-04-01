da277470f7d14fae3188b5bd5f8ef2f20cb5aa54
Fernando Henrique <fernandodojo@gmail.com>
2018-03-31 23:16:43 -0300

    Multiple meteors controlled by function rand ()

--- Exercícios/Meteoros/main.cpp
+++ Exercícios/Meteoros/main.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main()
 {
-    int i, j, l, k,  pontos=50;
-    int px = 20, py=11;         //coordenadas x e y do player
-    int ran, ran2;                     //numero aleatorio
-    int bx=0, by=0;             //coordenadas x e y da bala
-    char tela[22][22], tecla;   //matriz de coordenadas e tecla de movimento
-    int meteor[2][3];
+    int i, j, l, k,  score=50, contadormeteoro=0;      //indices, contagem de score
+    int ran, ran2;                  //numero aleatorio
+    int playerx = 20, playery=11;   //coordenadas x e y do player
+    int bulletx=21, bullety=21;       //coordenadas x e y da bala
+    char map[22][22], key;          //matriz de coordenadas e key de movimento
+    int meteor[2][3];               //matriz de coordenadas dos meteoros
 
     for (i=0; i<2; i++) // matriz de coordenadas dos meteoros / fila 0 responsavel por guardar as coordenadas y, movimento descendente. fila 1 responsavel por guardar coordenadas aleatorias x.
     {
@@ -23,10 +23,13 @@ int main()
 
     do{
         system("clear");
-        cout<<"pontos = "<<pontos;
+        cout<<"score = "<<score;
 
-        ran = rand()%(3)+1;//gerador aleatorio entre 3 e 1 q difine quantos meteoros seram acrescentados
-        ran2 = rand()%(20)+2;//gerador aleatorio q define a coordenada x de posição do meteoro
+        if (contadormeteoro <= 2) //controle pra regerar numero aleatorio
+            ran = rand()%(3)+1;//gerador aleatorio entre 3 e 1 q difine quantos meteoros seram acrescentados
+
+
+        ran2 = rand()%(19)+2;//gerador aleatorio q define a coordenada x de posição do meteoro
 
 
         //IMPRESSAO MATRIZ METEOR
@@ -44,10 +47,11 @@ int main()
 
         for (i=0; i<1; i++)
         {
-            for(j=0; j<ran; j++)//usa-se a variavel ran, para limitar a geração de um novo meteoro ////melhorar////
+            for(j=0; j<3; j++)//usa-se a variavel ran, para limitar a geração de um novo meteoro ////melhorar////
             {
-                if (meteor[0][j]==0)//se a matriz de meteoro estiver com 0 esta automaticamente iniciara a coordenada y com 1 e coordenada x aleatoria
+                if (meteor[0][j]==0 && contadormeteoro<=ran)//se a matriz de meteoro estiver com 0 esta automaticamente iniciara a coordenada y comř 1 e coordenada x aleatoria
                 {
+                    contadormeteoro++; //acrescenta na contagem de meteoros na tela
                     meteor[0][j]=1;
                     meteor[1][j]=ran2;
                     break; //limita o aparecimento de apenas um meteoro por execução
@@ -66,89 +70,90 @@ int main()
             {
                 if (meteor[i][j]!=0)
                     meteor[i][j] = meteor[i][j] + 1;// permiti-se o movimento descendente do meteoro caso o mesmo não esteja na coordenada 0
-                if (meteor[0][j]==20) //se o meteoro alcançar o fim da tela, ele retorna pra sua coordenada padrão 0,0, e subtrai-se 5 pontos
+                if (meteor[0][j]==21) //se o meteoro alcançar o fim da map, ele retorna pra sua coordenada padrão 0,0, e subtrai-se 5 score
                 {
-                    meteor[0][j]=0;
+                    contadormeteoro--; //adiciona na contagem do numero de meteoros na tela
+                    meteor[0][j]=0; //reseta o meteoro pra coordenada padrão
                     meteor[1][j]=0;
-                    pontos = pontos -5;
+                    score = score -5;
                 }
 
-                if (((bx>=meteor[0][j]-1) && (bx<=meteor[0][j]+1)) && (by==meteor[1][j])) //condições para detecção de coolisão, e acrescimento de potução. Verifica-se a posição x = X da bala e meteoro, verifica-se Y-1>y e y<Y+1
+                if ((bulletx>=meteor[0][j]-1) && (bulletx<=meteor[0][j]+1) && (bullety==meteor[1][j])) //condições para detecção de coolisão, e acrescimento de potução. Verifica-se a posição x = X da bala e meteoro, verifica-se Y-1>y e y<Y+1
                 {
-                    pontos = pontos + 3;
-                    bx=0;
-                    by=0;
-                    meteor[0][j]=0;
-                    meteor[1][j]=0;
+                    score = score + 3;
+                    bulletx=21; //coordenada padrão da bala
+                    bullety=21; //coordenada padrão da bala
+                    meteor[0][j]=0; //coordenada padrão do meteoro
+                    meteor[1][j]=0; //coordenada padrão do meteoro
+                    contadormeteoro--; //decremento na contagem de meteoros na tela
                 }
             }
         }
 
-
-
-        if(bx<=1)   //desaparece com a bala caso a mesma atinja o topo da tela
+        cout<<contadormeteoro<<"\n";
+        if(bulletx<=1)   //desaparece com a bala caso a mesma atinja o topo da map
         {
-            bx=0;
-            by=0;
+            bulletx=21;
+            bullety=21;
         }
 
         for(i=0; i<22; i++)
         {
             for (j=0; j<22; j++)
             {
-                if (i==21 || j==0 || i==0 || j==21) //imprime bordas da tela
-                    tela[i][j] ='#';
+                if (i==21 || j==0 || i==0 || j==21) //imprime bordas da map
+                    map[i][j] ='#';
                 else
-                    tela[i][j] =' ';//preenche qualquer coisa q não seja a borda com espaços
+                    map[i][j] =' ';//preenche qualquer coisa q não seja a borda com espaços
 
                 for (k=0; k<2; k++)
                 {
                     for(l=0; l<3; l++)
                     {
-                        tela[meteor[0][l]][meteor[1][l]] = '*';//define a posição do meteoro em todas as interações
+                        map[meteor[0][l]][meteor[1][l]] = '*';//define a posição do meteoro em todas as interações
                     }
                 }
 
-                tela[bx][by] = '|';//define a posição da bala em todas as interações
-                tela[px][py]= '_';// define a posição do player em todas as interações
+                map[bulletx][bullety] = '|';//define a posição da bala em todas as interações
+                map[playerx][playery]= '_';// define a posição do player em todas as interações
 
-                cout<<tela[i][j]; //impressão da matriz inteira
+                cout<<map[i][j]; //impressão da matriz inteira
             }
             cout<<"\n";
         }
-        cin>>tecla; // recebimento da letra para realizar uma ação pré programada
+        cin>>key; // recebimento da letra para realizar uma ação pré programada
 
-        //TELETRANPORTE AO SAIR DA TELA
-        if (py<=1)
-           py=20;
-        if (py>=21)
-            py=1;
-        //TELETRANPORTE AO SAIR DA TELA
+        //TELETRANPORTE AO SAIR DA map
+        if (playery<=1)
+           playery=19;
+        if (playery>=20)
+            playery=1;
+        //TELETRANPORTE AO SAIR DA map
 
-        //TECLAS DE MOVIMENTO DO PLAYER
-        if (tecla=='a')
-            py = py-1;
-        if (tecla=='d')
-            py = py+1;
-        if (tecla=='s')
-            py = py;
-        //TECLAS DE MOVIMENTO DO PLAYER
+        //keyS DE MOVIMENTO DO PLAYER
+        if (key=='a')
+            playery = playery-1;
+        if (key=='d')
+            playery = playery+1;
+        if (key=='s')
+            playery = playery;
+        //keyS DE MOVIMENTO DO PLAYER
 
-        //TECLAS DE MOVIMENTO DA BALA
-        if (tecla =='l')
+        //keyS DE MOVIMENTO DA BALA
+        if (key =='l')
         {
-            pontos = pontos-2;
-            bx=20;
-            by=py;
-            bx = bx-1;
+            score = score-2;
+            bulletx=20;
+            bullety=playery;
+            //bulletx = bulletx-1;
         }
-        bx = bx-1;
-        //TECLAS DE MOVIMENTO DA BALA
+        if (bullety!=21)
+            bulletx = bulletx-1;
+        //keyS DE MOVIMENTO DA BALA
 
     }
-    while(pontos>0);
+    while(score>0);
 
-    if(pontos<=0)
+    if(score<=0)
         cout<<"Game Over";
-}
 
