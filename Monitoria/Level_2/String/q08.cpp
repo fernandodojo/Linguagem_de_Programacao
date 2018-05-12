58d35f293a2546fc3c6fd7574cd82fe33231adee
Fernando Henrique <fernandodojo@gmail.com>
2018-05-12 17:45:09 -0300

    Q08 - Strings
    conditions fix

--- Monitoria/Level_2/String/q08.cpp
+++ Monitoria/Level_2/String/q08.cpp
@@ -10,48 +10,47 @@ int main()
 	char *data;
 	char *achado;
 	char **vetor;
-	char *diapont, *mespont, *anopont;
 	int dia, mes, ano;
 	int i, cont=0;
 
 	data = new char[20];
+	cout<<"Digite uma data seguindo o formato DD/MM/AAAA , DD/MM/YYYY\n";
 	cin.getline(data, 20);
 
-	for (i=0; i<strlen(data); i++)
-	{
-		if (isdigit(data[i]))
-			cont++;
-	}
+    if(data[2]=='/' && data[5]=='/')        //verifica se o formato da data digitado obedece o formato requisitado.
+    {
+        for (i=0; i<strlen(data); i++)      //verifica e conta a quantidade de digitos que são numeros
+        {
+            if (isdigit(data[i]))           //verificação se é um digito numérico
+                cont++;
+        }
 
-	vetor = new char*[cont]; //ALOCAÇÃO DINAMICA DE UM VETOR DE PONTEIROS
+        if(cont==8)                         //verifica-se se existem de fato 8 numeros na data
+        {
+            vetor = new char*[3];           //alocação de um vetor de ponteiro com 3 posições
 
-	//for (i=0; i<cont; i++)
-		//vetor[i] = new char[100]; //ALOCAÇÃO DINAMICA DE STRINGS
+            achado = strtok(data,"/");      //achado aponta para inicio da posicao da string antes da barra
+            i=0;
+            while(achado!=NULL)
+            {
+                vetor[i]= achado;           //o vetor recebe os valores que se encontram entre as barras, cada parte da data entra em uma unica posição do vetor
+                achado = strtok(NULL, "/"); //continua-se a verificar a existencia de novas barras, caso encontre o retorno é um ponteiro para posição de tudo ente barras
+                i++;
+            }
 
-	achado = strtok(data,"/");
-	i=0;
-	while(achado!=NULL)
-	{
-		vetor[i]= achado;
-		//cout<<achado<<"\n";
-		achado = strtok(NULL, "/");
-		i++;
-	}
+            dia	= atoi(vetor[0]);           //conversao de cada posicao do vetor em um inteiro
+            mes	= atoi(vetor[1]);
+            ano	= atoi(vetor[2]);
 
-	//for(i=0; i<cont; i++)
-	//	cout<<vetor[i]<<" ";
+            cout<<"dia "<<dia<<"\n";
+            cout<<"mes "<<mes<<"\n";
+            cout<<"ano "<<ano<<"\n";
+        }
+        else
+            cout<<"Data inválida, encerrando...\n";
+    }
+	else
+        cout<<"Data inválida, encerrando...\n";
 
-	diapont = vetor[0];
-	mespont = vetor[1];
-	anopont = vetor[2];
-
-	dia	= atoi(diapont);
-	mes	= atoi(mespont);
-	ano	= atoi(anopont);
-	cout<<"dia "<<dia<<"\n";
-	cout<<"mes "<<mes<<"\n";
-	cout<<"ano "<<ano<<"\n";
-
-	return 0;
-
+    return 0;
 }
